106f8507316b5b6e77dea4f44e7fe7a44ca37504
MrSkorohod <mr.vl.skorohod@gmail.com>
2023-09-07 18:49:18 +0300

    Added enviroment cofiguration

--- apps/weather_app/src/app/shared/search-bar/search-bar.component.ts
+++ apps/weather_app/src/app/shared/search-bar/search-bar.component.ts
@@ -1,5 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  SimpleChanges,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,7 +14,36 @@ import { FormControl, FormGroup } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchBarComponent {
+  private searchInputValueSubject = new BehaviorSubject<string>('');
+
+  constructor(private http: HttpClient) {}
+
   testForm = new FormGroup({
     testValue: new FormControl(''),
   });
+
+  async getCities(cityName: string) {
+    console.log('cityName: ', cityName);
+
+    const response = await firstValueFrom(
+      this.http.get<any>(
+        `https://api.api-ninjas.com/v1/city?name=${cityName}`,
+        {
+          headers: { 'X-Api-Key': process.env['API_KEY'] as string },
+        }
+      )
+    );
+
+    console.log('response: ', response);
+  }
+
+  inputChange(event: any): void {
+    const cityName = this.searchInputValueSubject.getValue();
+
+    this.searchInputValueSubject.next(cityName + event?.data);
+
+    if (cityName.length > 3) {
+      this.getCities(this.searchInputValueSubject.getValue());
+    }
+  }
 }
