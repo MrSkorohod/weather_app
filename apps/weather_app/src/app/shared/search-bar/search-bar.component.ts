97930b04cf8ae54b58612bfb400c8c3fde3956e1
MrSkorohod <mr.vl.skorohod@gmail.com>
2023-09-12 09:50:58 +0300

    This feature added enviroment configuration. Also added webpack config file and envs files

--- apps/weather_app/src/app/shared/search-bar/search-bar.component.ts
+++ apps/weather_app/src/app/shared/search-bar/search-bar.component.ts
@@ -1,11 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  SimpleChanges,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -14,36 +8,7 @@ import { BehaviorSubject, Observable, firstValueFrom } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchBarComponent {
-  private searchInputValueSubject = new BehaviorSubject<string>('');
-
-  constructor(private http: HttpClient) {}
-
   testForm = new FormGroup({
     testValue: new FormControl(''),
   });
-
-  async getCities(cityName: string) {
-    console.log('cityName: ', cityName);
-
-    const response = await firstValueFrom(
-      this.http.get<any>(
-        `https://api.api-ninjas.com/v1/city?name=${cityName}`,
-        {
-          headers: { 'X-Api-Key': process.env['API_KEY'] as string },
-        }
-      )
-    );
-
-    console.log('response: ', response);
-  }
-
-  inputChange(event: any): void {
-    const cityName = this.searchInputValueSubject.getValue();
-
-    this.searchInputValueSubject.next(cityName + event?.data);
-
-    if (cityName.length > 3) {
-      this.getCities(this.searchInputValueSubject.getValue());
-    }
-  }
 }
