0f023ea5fad80b3b9054122e831813c59be195e7
MrSkorohod <mr.vl.skorohod@gmail.com>
2023-10-16 18:08:29 +0300

    Add configuration for test

--- apps/weather_app/src/app/core/api/api-general.service.spec.ts
+++ apps/weather_app/src/app/core/api/api-general.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiGeneralService } from './api-general.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ApiGeneralService', () => {
   let service: ApiGeneralService;
+  let http: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    http = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: http,
+        },
+      ],
+    });
     service = TestBed.inject(ApiGeneralService);
   });
 
--- apps/weather_app/src/app/modules/home-page/home-page.component.spec.ts
+++ apps/weather_app/src/app/modules/home-page/home-page.component.spec.ts
@@ -1,13 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomePageComponent } from './home-page.component';
+import { CitiesService } from '../../core/api/cities.service';
+import { of } from 'rxjs';
+import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let citiesService: jasmine.SpyObj<CitiesService>;
 
   beforeEach(async () => {
+    citiesService = jasmine.createSpyObj('citiesService', {
+      getCityByCoords: of([]),
+    });
     await TestBed.configureTestingModule({
-      declarations: [HomePageComponent],
+      declarations: [HomePageComponent, SearchBarComponent],
+      providers: [
+        {
+          provide: CitiesService,
+          useValue: citiesService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePageComponent);
