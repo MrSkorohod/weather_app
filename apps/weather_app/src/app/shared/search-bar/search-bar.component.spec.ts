29df5bab5eece6e5db0c01c315911b2aa7599daf
MrSkorohod <mr.vl.skorohod@gmail.com>
2023-10-17 16:35:26 +0300

    Add test on 'search-bar' component

--- apps/weather_app/src/app/shared/search-bar/search-bar.component.spec.ts
+++ apps/weather_app/src/app/shared/search-bar/search-bar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
 import { SharedModule } from '../shared.module';
+import { By } from '@angular/platform-browser';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -19,5 +20,47 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have static placeholder', () => {
+    const taigaInput = fixture.nativeElement.querySelector('tui-input');
+    expect(taigaInput.textContent).toContain('Choose you city');
+  });
+
+  it('should correctly render the passed @Input value', () => {
+    component.value = 'Test value';
+
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.value).toEqual('Test value');
+  });
+
+  it('should input event work correctly', () => {
+    fixture.detectChanges();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    const elem = input.nativeElement;
+
+    expect(elem.value).toBe('');
+
+    elem.value = 'Test value';
+    elem.dispatchEvent(new Event('input'));
+
+    expect(fixture.componentInstance.value).toBe('Test value');
+  });
+
+  it('should emit value', () => {
+    spyOn(component.valueChange, 'emit');
+
+    const input = fixture.debugElement.query(By.css('input'));
+
+    input.triggerEventHandler('input', {
+      target: { value: 'Input Test Value' },
+    });
+
+    fixture.detectChanges();
+
+    expect(component.valueChange.emit).toHaveBeenCalled();
+    expect(component.valueChange.emit).toHaveBeenCalledWith('Input Test Value');
   });
 });
